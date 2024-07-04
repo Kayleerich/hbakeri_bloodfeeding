b47c5bce6d0f0d38a2d65e201ae4d87d7969b13c
Kayleerich <93557771+Kayleerich@users.noreply.github.com>
2024-07-04 13:55:33 -0600

    Update DESeq2_hbak_analysis.R

--- DESeq2_hbak_analysis.R
+++ DESeq2_hbak_analysis.R
@@ -5,8 +5,13 @@ library(PCAtools)
 
 setwd("C:\\path\\to\\files")
 
+## select subset from data, if FALSE: all data, if TRUE: excludes day 6 data
+d7_only <- TRUE
+
+## set default variables
 out_file <- "hbak_PRJEB15396.WBPS18_DESeq2_LightvDark_results.tsv"
-mat_file <- "featureCounts_d35s0\\hbak_PRJEB15396.WBPS18_featurecounts_d35s0.Rmatrix.tsv"
+desc <- "Days 6 and 7, condition Light vs. Dark"
+mat_file <- "hbak_PRJEB15396.WBPS18_featurecounts.Rmatrix.tsv"
 info_file <- "sample_table.tsv"
 levels <- c("Dark", "Light")
 
@@ -21,6 +26,13 @@ cts <- read.delim(mat_file)
 row.names(cts) <- cts$Geneid
 cts$Geneid <- NULL
 
+## change variables and dataframe content if subsetting data 
+if(d7_only == TRUE){
+  cts <- as.matrix(cts %>% select(contains("D7")))
+  coldata <- coldata %>% filter(str_detect(day, "D7"))
+  out_file <- "hbak_PRJEB15396.WBPS18_DESeq2_LightvDark_results_D7Only.tsv"
+  desc <- "Day 7 only, condition Light vs. Dark"
+}
 
 ## check if sample identifiers from count data match sample info
 all(rownames(coldata) %in% colnames(cts))
@@ -40,7 +52,9 @@ dds <- dds[rowSums(counts(dds)) > 0,]
 
 ## get results, change pval cut-off to 0.05 instead of (default) 0.1
 res <- results(dds, alpha=0.05)
+print(desc)
 summary(res)
+# Day 7 only, condition: Light vs. Dark
 # out of 23631 with nonzero total read count
 # adjusted p-value < 0.05
 # LFC > 0 (up)       : 389, 1.6%
@@ -49,6 +63,14 @@ summary(res)
 # low counts [2]     : 5495, 23%
 # (mean count < 10)
 
+# Days 6 and 7, condition: Light vs. Dark
+# out of 23631 with nonzero total read count
+# adjusted p-value < 0.05
+# LFC > 0 (up)       : 389, 1.6%
+# LFC < 0 (down)     : 162, 0.69%
+# outliers [1]       : 645, 2.7%
+# low counts [2]     : 5476, 23%
+# (mean count < 10)
 
 resOrdered <- res[order(res$padj),]
 head(resOrdered[ order(resOrdered$padj, decreasing = FALSE), ], 75)
